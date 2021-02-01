5a14bdd3958e9a0497658aa770b8e4845ff379d6
Erick Garcia <74308231+eggarcia14@users.noreply.github.com>
2021-02-01 18:43:54 -0500

    Delete src directory

--- src/init/FirebaseProvider.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Firebase from './firebase';
-
-export const FirebaseContext = React.createContext(); 
-export const FirebaseProvider = (props) => ( 
-<FirebaseContext.Provider value={new Firebase()}> 
-{props.children} 
-</FirebaseContext.Provider> 
-);
